d7d86fa018d199785d1d19036e1d233703ff5cf2
vukicuki <113440915+vukicuki@users.noreply.github.com>
2022-09-14 19:02:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-7eusez
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-7eusez)
---
+++ index.js
@@ -0,0 +1,34 @@
+//let a=5;
+//a=a+1;
+//let b=17;
+//b=a-1;
+//console.log(a);
+//console.log(b);
+
+// let a =!!"abc73f";
+// console.log(a);
+
+// let a = promp('Unesi broj');
+// console.log(a * a);
+
+// if () {}
+
+// let a = '5';
+// if(typeof a=="number") {console.log (a*a);}
+// console.log(typeof(typeof a));
+
+// let a=Number(promp("Vase godine?"))
+// if (a>=18){console.log(Punoletni ste);}
+// else{console.log("Niste punoletni")}
+
+let god =Number(prompt("Unesite godine"));
+if(Number.isNaN(god)){
+ console.log("Punoletni ste");
+ }
+ if(god>=18){
+ console.log("Niste punoletni");
+}else{
+console.log("Niste uneli broj!");
+ } 
+
+
